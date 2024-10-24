2215e528bc2a590675fb1a1a268e16a777979e53
Kevin Solorzanov <solorzanovillacortakevinpatric@gmail.com>
2024-10-24 00:09:48 -0500

    Estructura inicial

---
+++ src/main/java/pe/com/cibertec/EF_LP2/EfLp2Application.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.EF_LP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfLp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfLp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/EF_LP2/EfLp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.EF_LP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfLp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
